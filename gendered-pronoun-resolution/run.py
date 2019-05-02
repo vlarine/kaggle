4fdac28ebbe9086a54ce81547646f3012be2ceaf
Yury Kashnitskiy <yury.kashnitskiy@kpn.com>
2019-05-02 15:21:00 +0200

    add fully reproducible solution~; git push

--- gendered-pronoun-resolution/run.py
+++ gendered-pronoun-resolution/run.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 def preprocess_embeddings():
 
     bert_gap = BertGAP(
-            bert_model='../models/uncased_L-24_H-1024_A-16',
+            bert_model='models/uncased_L-24_H-1024_A-16',
             emb_size=1024,  # BERT Large embeddings size
             seq_len=64,     # Tokens window size to trocess embeddings 
             n_layers=3,     # Number of output embeddings layers to take 
@@ -19,23 +19,23 @@ def preprocess_embeddings():
             normalize_text=True)
 
     bert_gap.process_embeddings(
-            input_file = '../gap-coreference/gap-test.tsv',
-            output_file = '../data/emb_uncased_test.json')
+            input_file = 'gap-coreference/gap-test.tsv',
+            output_file = 'input/emb_uncased_test.json')
 
     bert_gap.process_embeddings(
-            input_file = '../gap-coreference/gap-validation.tsv',
-            output_file = '../data/emb_uncased_validation.json')
+            input_file = 'gap-coreference/gap-validation.tsv',
+            output_file = 'input/emb_uncased_validation.json')
 
     bert_gap.process_embeddings(
-            input_file = '../gap-coreference/gap-development.tsv',
-            output_file = '../data/emb_uncased_development.json')
+            input_file = 'gap-coreference/gap-development.tsv',
+            output_file = 'input/emb_uncased_development.json')
 
     bert_gap.process_embeddings(
-            input_file = '../input/test_stage_1.tsv',
-            output_file = '../data/emb_uncased_test_stage_1.json')
+            input_file = 'input/test_stage_1.tsv',
+            output_file = 'input/emb_uncased_test_stage_1.json')
 
     bert_gap = BertGAP(
-            bert_model='../models/cased_L-24_H-1024_A-16',
+            bert_model='models/cased_L-24_H-1024_A-16',
             emb_size=1024,
             seq_len=64,
             n_layers=3,
@@ -44,40 +44,43 @@ def preprocess_embeddings():
             normalize_text=True)
 
     bert_gap.process_embeddings(
-            input_file = '../gap-coreference/gap-test.tsv',
-            output_file = '../data/emb_cased_test.json')
+            input_file = 'gap-coreference/gap-test.tsv',
+            output_file = 'input/emb_cased_test.json')
 
     bert_gap.process_embeddings(
-            input_file = '../gap-coreference/gap-validation.tsv',
-            output_file = '../data/emb_cased_validation.json')
+            input_file = 'gap-coreference/gap-validation.tsv',
+            output_file = 'input/emb_cased_validation.json')
 
     bert_gap.process_embeddings(
-            input_file = '../gap-coreference/gap-development.tsv',
-            output_file = '../data/emb_cased_development.json')
+            input_file = 'gap-coreference/gap-development.tsv',
+            output_file = 'input/emb_cased_development.json')
 
     bert_gap.process_embeddings(
-            input_file = '../input/test_stage_1.tsv',
-            output_file = '../data/emb_cased_test_stage_1.json')
+            input_file = 'input/test_stage_1.tsv',
+            output_file = 'input/emb_cased_test_stage_1.json')
 
 
 def preprocess():
-
-    # Create dirs
-    for dir_to_create in ['../data/', '../features']:
-        if not os.path.exists(dir_to_create):
-            os.mkdir(dir_to_create)
-
+    
+    # Create necessary dirs and downloads BERT and ELMo
+    #os.system('sh preparation.sh')
+    
     # Create test/train DFs
     train_dfs = []
-    for filename in ['../gap-coreference/gap-test.tsv', '../gap-coreference/gap-validation.tsv']:
+    for filename in ['gap-coreference/gap-test.tsv', 
+                     'gap-coreference/gap-validation.tsv']:
         df = pd.read_csv(filename, sep='\t')
         train_dfs.append(df)
     train_df = pd.concat(train_dfs, ignore_index=True)
-    train_df.to_csv('../input/train.tsv', sep='\t', index=False)
-    shutil.copyfile('../gap-coreference/gap-development.tsv', '../input/test.tsv')
+    train_df.to_csv('input/train.tsv', sep='\t', index=False)
+    shutil.copyfile('gap-coreference/gap-development.tsv', 'input/test.tsv')
+    
+
+    # Create features
+    #os.system('python3 scripts/build_features.py')
 
     # Create embeddings
-    preprocess_embeddings()
+    #preprocess_embeddings()
 
 
 def train_model():
@@ -87,32 +90,32 @@ def train_model():
     embeddings = [
         {
             'train': [
-                '../data/emb_uncased_test.json',
-                '../data/emb_uncased_validation.json'
+                'input/emb_uncased_test.json',
+                'input/emb_uncased_validation.json'
                 ],
-            'test': ['../data/emb_uncased_test_stage_1.json', ]
+            'test': ['input/emb_uncased_test_stage_1.json', ]
         },
         {
             'train': [
-                '../data/emb_cased_test.json',
-                '../data/emb_cased_validation.json'
+                'input/emb_cased_test.json',
+                'input/emb_cased_validation.json'
                 ],
-            'test': ['../data/emb_cased_test_stage_1.json', ]
+            'test': ['input/emb_cased_test_stage_1.json', ]
         },
     ]
 
     # Precomputed features .tsv files
     features = {
-        'train': ['../features/train_features.tsv', ],
-        'train_ids': ['../input/train.tsv', ],
-        'test': ['../features/test_features.tsv', ],
-        'test_ids': ['../input/test.tsv', ],
+        'train': ['features/train_features.tsv', ],
+        'train_ids': ['input/train.tsv', ],
+        'test': ['features/test_features.tsv', ],
+        'test_ids': ['input/test.tsv', ],
     }
 
     # Input dataset files to get labels.
     input_files = {
-        'train': ['../gap-coreference/gap-test.tsv', '../gap-coreference/gap-validation.tsv'],
-        'test': ['../gap-coreference/gap-development.tsv', ]
+        'train': ['gap-coreference/gap-test.tsv', 'gap-coreference/gap-validation.tsv'],
+        'test': ['gap-coreference/gap-development.tsv', ]
     }
 
     model.train_model(embeddings=embeddings, features=features, input_files=input_files)
